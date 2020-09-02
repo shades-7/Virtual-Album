6210f05bbdc2c89d13f5c23e6a787d09f20fd5e9
shades-7 <salonimark7@gmail.com>
2020-09-02 12:23:20 +0530

    second commit

--- src/Component/photopage/photopage.js
+++ src/Component/photopage/photopage.js
@@ -5,6 +5,14 @@ class photopage extends Component {
     render(){
         return(
         <div className={classes.container}>
+        <div className={classes.navhor}></div>
+        <div className={classes.hor}>
+         <div className={classes.navver}></div>
+         <div className={classes.align}></div>
+         <div className={classes.picbox}></div>
+        </div>
+        
+       
             
         </div>
         )
